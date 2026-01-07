55abb9604c8a294d69814cc9c9df8840d83cb87d
FlazeFy <flazen.edu@gmail.com>
2026-01-07 09:52:28 +0700

    [BE] Migration for Model : Dictionary, User, Family, History, Allergic, Hobby, Task

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
 import express, { Request, Response } from "express"
-import path from "path"
 
 const PORT = 8005
 
 const app = express()
 app.use(express.json())
-const productPath = path.join(process.cwd(), '/src/db/products.json')
 
 app.get("/", (req: Request, res: Response) => {
     res.status(200).send({message:"hello world"})
