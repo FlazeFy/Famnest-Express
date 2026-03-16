fc7bea1442dfb4d63cd700c25638d65788b94eb7
FlazeFy2 <flazen.edu@gmail.com>
2026-03-17 04:32:38 +0700

    docs(swagger) : add API docs for GET meal list, GET history list, and GET total meals grouped by context

--- src/routes/history.router.ts
+++ src/routes/history.router.ts
@@ -17,7 +17,43 @@ export default class HistoryRouter {
     private initializeRoute = () => {
         const { getAllHistoryController, hardDeleteHistoryByIdController, exportHistoryController, hardDeleteAllHistoryController } = this.historyController
 
+        /**
+         * @openapi
+         * /api/v1/histories:
+         *   get:
+         *     summary: Get history list
+         *     tags: [History]
+         *     security:
+         *       - bearerAuth: []
+         *     responses:
+         *       200:
+         *         description: Get history successful
+         *         content:
+         *           application/json:
+         *             schema:
+         *               type: object
+         *               properties:
+         *                 message: { type: string, example: Get history successful }
+         *                 data:
+         *                   type: array
+         *                   items:
+         *                     type: object
+         *                     properties:
+         *                       id: { type: string, format: uuid, example: eb3e0758-f94e-4178-91c5-088be51181d0 }
+         *                       history_type: { type: string, example: yum }
+         *                       history_context: { type: string, example: abnormally shrilly }
+         *                       created_by: { type: string, format: uuid, example: 623135dd-54f0-42c8-ae53-72ce57e813c8 }
+         *                       created_at: { type: string, format: date-time, example: 2026-01-29T23:39:42.120Z }
+         *                 meta:
+         *                   type: object
+         *                   properties:
+         *                     page: { type: integer, example: 1 }
+         *                     limit: { type: integer, example: 14 }
+         *                     total: { type: integer, example: 9 }
+         *                     total_page: { type: integer, example: 1 }
+         */
         this.route.get("/", verifyAuthToken, authorizeRole(["admin","user"]), getAllHistoryController)
+
         this.route.get("/export", verifyAuthToken, authorizeRole(["admin","user"]), exportHistoryController)
         this.route.delete("/:id", verifyAuthToken, authorizeRole(["user"]), validateParamMiddleware(templateIdParamSchema), hardDeleteHistoryByIdController)
         this.route.delete("/", verifyAuthToken, authorizeRole(["user"]), hardDeleteAllHistoryController)
